e582d89cd8629b28a98284df582e3ab3bc0a1e19
Nigel <nigel.monsanto@gmail.com>
2020-12-24 17:53:10 +0800

    Removed navbar, changed style of #app

--- initial_project/tempo_vuejs/src/App.vue
+++ initial_project/tempo_vuejs/src/App.vue
@@ -1,17 +1,6 @@
 <template>
     <div id="app">
-        <nav class="navbar navbar-expand navbar-dark bg-dark">
-            <router-link to="/" class="navbar-brand">Tempo</router-link>
-            <div class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <router-link to="/tasks" class="nav-link">Tasks</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/add" class="nav-link">Add</router-link>
-                </li>
-            </div>
-        </nav>
-        <div class="container mt-3">
+        <div class="container mt-5">
             <router-view />
         </div>
     </div>
@@ -25,11 +14,7 @@ export default {
 
 <style>
     #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
         text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
+        width: 100%;
     }
 </style>
